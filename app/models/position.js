7d1f806c9d7b98ceddbd45968132a5871d1ee79b
allthesignals <wilburnforce@gmail.com>
2017-03-28 14:50:33 -0400

    Adds contact info;
    Cleans up clustering a bit more;
    Closes #6

--- app/models/position.js
+++ app/models/position.js
@@ -6,6 +6,11 @@ export default DS.Model.extend({
   category: DS.attr('string'),
   site_name: DS.attr('string'),
   title: DS.attr('string'),
+  external_application_url: DS.attr('string'),
+  primary_contact_person: DS.attr('string'),
+  primary_contact_person_title: DS.attr('string'),
+  primary_contact_person_phone: DS.attr('string'),
+  site_phone: DS.attr('number'),
   duties_responsbilities: DS.attr('string'),
   ideal_candidate: DS.attr('string'),
   open_positions: DS.attr('number'),
@@ -13,3 +18,11 @@ export default DS.Model.extend({
   applicants: DS.hasMany('applicant'),
   isSelected: false
 });
+
+
+ 
+ 
+ 
+ 
+ 
+ 
